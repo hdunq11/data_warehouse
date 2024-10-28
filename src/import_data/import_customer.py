034c551d7ecf73ecc6813fc665aee30066a2f767
hdunq11 <dunghoang080804@gmail.com>
2024-10-28 19:53:16 +0700

    fix dob of customer

--- src/import_data/import_customer.py
+++ src/import_data/import_customer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from src import settings
 from src.db import db
-
+from datetime import datetime, timedelta
 customer_db = db["customers"]
 
 def import_customer():
@@ -16,14 +16,23 @@ def import_customer():
     # Extract unique customers
     customers = df[['customerid', 'DOB', 'gender', 'address', 'Website', 'job', 'industry']].drop_duplicates()
 
-    # Convert to dictionary
+  
+    if pd.api.types.is_numeric_dtype(customers['DOB']):
+       
+        customers['DOB'] = pd.to_datetime('1899-12-30') + pd.to_timedelta(customers['DOB'], 'D')
+    else:
+       
+        customers['DOB'] = pd.to_datetime(customers['DOB'], errors='coerce')
+
+    customers['DOB'] = customers['DOB'].dt.strftime('%d-%m-%Y')
+
     customers_dict = customers.to_dict(orient='records')
 
-    print(customers_dict[:5])
-    print(len(customers_dict))
+    print(customers_dict[:5])  
+    print(len(customers_dict))  
 
     for customer in customers_dict:
         try:
             customer_db.insert_one(customer)
-        except:
-            pass
+        except Exception as e:
+            print(f"Lỗi khi chèn dữ liệu: {e}")  # In ra lỗi nếu có
