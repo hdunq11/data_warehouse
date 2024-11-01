f8f959b161e1794a74dd24516a43e2567a9fa4a7
hdunq11 <dunghoang080804@gmail.com>
2024-11-01 12:18:06 +0700

    update query

--- src/query.py
+++ src/query.py
@@ -5,24 +5,24 @@ from src.query_data.c4_total_popcorn_sales_by_weekday_hour import c4_total_popco
 from src.query_data.c5_ticket_sales_by_day import c5_ticket_sales_by_day
 
 if __name__ == "__main__":
-    print("Running c1_rev_by_hour...")
-    c1_rev_by_hour()
-    print("Completed c1_rev_by_hour\n")
+    # print("Running c1_rev_by_hour...")
+    # c1_rev_by_hour()
+    # print("Completed c1_rev_by_hour\n")
 
-    print("Running c2_rev_by_weekday_hour...")
-    c2_rev_by_weekday_hour()
-    print("Completed c2_rev_by_weekday_hour\n")
+    # print("Running c2_rev_by_weekday_hour...")
+    # c2_rev_by_weekday_hour()
+    # print("Completed c2_rev_by_weekday_hour\n")
 
-    print("Running c3_rev_by_ticket_type_weekday...")
-    c3_rev_by_ticket_type_weekday()
-    print("Completed c3_rev_by_ticket_type_weekday\n")
+    # print("Running c3_rev_by_ticket_type_weekday...")
+    # c3_rev_by_ticket_type_weekday()
+    # print("Completed c3_rev_by_ticket_type_weekday\n")
 
     print("Running c4_total_popcorn_sales_by_weekday_hour...")
     c4_total_popcorn_sales_by_weekday_hour()
     print("Completed c4_total_popcorn_sales_by_weekday_hour\n")
 
-    print("Running c5_ticket_sales_by_day...")
-    c5_ticket_sales_by_day()
-    print("Completed c5_ticket_sales_by_day\n")
+    # print("Running c5_ticket_sales_by_day...")
+    # c5_ticket_sales_by_day()
+    # print("Completed c5_ticket_sales_by_day\n")
 
     print("All queries have been run successfully.")
